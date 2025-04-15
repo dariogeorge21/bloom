26a48ffacb341d188d99d83167829767bd7c3927
Dario George <edu.dariogeorge21@gmail.com>
2025-04-15 09:50:21 +0530

    maps location

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -166,7 +166,7 @@ export default function AboutPage() {
               
               <div className="rounded-xl overflow-hidden shadow-lg border border-gray-200">
                 <iframe 
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15713.828268077284!2d76.6658146!3d9.7027335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07cc042c4df9c7%3A0x9ef8d25fa70bb861!2sTabore%20Ashram%20Kodumpidi!5e0!3m2!1sen!2sin!4v1648675691548!5m2!1sen!2sin" 
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3931.8526270281227!2d76.71707817535686!3d9.778534990315858!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07cf43958d55e7%3A0x346bb31ca4cddecc!2sTabore%20Prarthanalayam%20Pala!5e0!3m2!1sen!2sin!4v1744683518243!5m2!1sen!2sin"
                   width="100%" 
                   height="450" 
                   style={{ border: 0 }} 
